9fb60c06cf2755011df23342fc7af27fdd18724a
pyavari <pyavari@bwc.org>
2012-07-29 14:23:15 +0300

    Can persist documents

--- MyFavourites/Models/Document.cs
+++ MyFavourites/Models/Document.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Castle.ActiveRecord;
 
 namespace MyFavourites.Models
 {
-    public class Document
+    [ActiveRecord]
+    public class Document : ActiveRecordBase<Document>
     {
+        [PrimaryKey]
+        public int Id { get; set; }
+
+        [Property]
         public string Title { get; set; }
+
+        [Property]
         public string Author { get; set; }
     }
 }
--- MyFavouritesTests/Models/DocumentTests.cs
+++ MyFavouritesTests/Models/DocumentTests.cs
@@ -3,24 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MyFavourites.Models;
+using MyFavouritesTests.Framework;
 using NUnit.Framework;
 
 namespace MyFavouritesTests.Models
 {
     [TestFixture]
-    public class DocumentTests
+    public class DocumentTests : ModelTests
     {
         [Test]
         public void CanPersist()
         {
             // Setup
             var document = new Document();
-
-            // Act
             document.Title = "New Findings";
             document.Author = "Payam Yavari";
 
+            // Act
+            document.Save();
+
             // Assert
+            document = Document.Find(document.Id);
             Assert.AreEqual("New Findings", document.Title);
             Assert.AreEqual("Payam Yavari", document.Author);
 
