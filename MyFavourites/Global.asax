3c48c9cb69e4f1a234bb0727656707d727f30911
pyavari <pyavari@bwc.org>
2012-07-29 09:43:17 +0300

    Researcher instance test

---
+++ MyFavourites/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyFavourites.MvcApplication" Language="C#" %>
---
+++ MyFavouritesTests/Models/ResearcherTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace MyFavouritesTests.Models
+{
+    [TestFixture]
+    public class ResearcherTests
+    {
+        [Test]
+        public void CanCreateAResearcher()
+        {
+            //setup/act
+            var researcher = new Researcher();
+
+            //Assert
+            Assert.IsInstanceOf<Researcher>(researcher);
+        }
+    }
+}
